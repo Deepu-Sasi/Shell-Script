3d67d418442bf0f38017053a220cdea905e2143f
Deepu Sasi <89693756+Deepu-Sasi@users.noreply.github.com>
2023-05-29 11:40:27 +0530

    Add files via upload

---
+++ Student details.sh
@@ -0,0 +1,15 @@
+echo "Enter your name :"
+read name
+echo "Enter the semester :"
+read semester
+echo "Enter the Batch :"
+read batch
+echo "Enter the Roll Number :"
+read roll
+echo "Enter the Register Number :"
+read register
+echo "Name :$name"
+echo "Semester :$semester"
+echo "Batch :$batch"
+echo "Roll Number :$roll"
+echo "Register Number :$register"
